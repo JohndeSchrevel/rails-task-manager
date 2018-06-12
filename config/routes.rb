19e59f453d89bbabe162a713448ec34c7075185f
Jonathan de Schrevel <jonathan.deschrevel@gmail.com>
2018-06-12 16:30:54 +0200

    task manager

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   get 'tasks/new', to: 'tasks#new', as: :new_task
   post 'tasks', to: 'tasks#create'
   get 'tasks/:id', to: 'tasks#show', as: :task
-  get 'tasks/:id/edit', to: 'tasks#edit'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
   patch 'tasks/:id', to: 'tasks#update'
-  match 'tasks/complete/:id', to: 'tasks#complete', as: 'complete_task', via: :put
-  match 'tasks/complete/:id', to: 'tasks#incomplete', as: 'incomplete_task', via: :delete
+  # match 'tasks/complete/:id', to: 'tasks#complete', as: 'complete_task', via: :put
+  # match 'tasks/complete/:id', to: 'tasks#incomplete', as: 'incomplete_task', via: :delete
   delete 'tasks/:id', to: 'tasks#destroy'
 end
