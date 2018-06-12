52d6a3c497201a37de859175d8a899d5ee594646
Jonathan de Schrevel <jonathan.deschrevel@gmail.com>
2018-06-12 15:19:06 +0200

    making action responsive

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
+  match 'tasks/complete/:id', to: 'tasks#complete', as: 'complete_task', via: :put
+  match 'tasks/complete/:id', to: 'tasks#incomplete', as: 'incomplete_task', via: :delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
